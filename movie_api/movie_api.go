d8f3b8c979e2906c45d70533b1e27b84bb31540d
dgreat91 <dgreat91@gmail.com>
2020-05-11 19:05:35 +0800

    Created a simple Webserver in  Main Class movie_api.go

--- movie_api/movie_api.go
+++ movie_api/movie_api.go
@@ -3,14 +3,27 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 
 	"github.com/dgreat91/rm_movieapi_task/configuration"
 )
 
+//Handle request /ping by responding with pong
+func ping(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("pong"))
+}
 func main() {
 	cfg, err := configuration.NewConfig()
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(cfg.API.Key)
+	fmt.Printf("Server is running on '%s' port '%s'", cfg.Server.Host, cfg.Server.Port)
+
+	//Request Handler For Request /ping
+	http.HandleFunc("/ping", ping)
+
+	//Start Web Server
+	if err := http.ListenAndServe(cfg.Server.Host+":"+cfg.Server.Port, nil); err != nil {
+		panic(err)
+	}
 }
